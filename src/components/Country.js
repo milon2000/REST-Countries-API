cbfb3b5b593a3c276dd7af20da510009a0c90f2b
milon2000 <milakozlowska2000@gmail.com>
2022-04-08 16:08:14 +0200

    style: home page

--- src/components/Country.js
+++ src/components/Country.js
@@ -8,10 +8,12 @@ const Country = ({country}) => {
                 <div className="country-flag-wrapper">
                     <img src={country.flags.png} alt={country.name.official} className="country-flag" />
                 </div>
+                <div className="country-details-wrapper">
                 <h3 className="country-name">{country.name.official}</h3>
                 <p className="country-details"><span className="details-bold">Population: </span>{country.population}</p>
                 <p className="country-details"><span className="details-bold">Region: </span>{country.region}</p>
                 <p className="country-details"><span className="details-bold">Capital: </span>{country.capital}</p>
+                </div>
             </div>
         </div>
     )
